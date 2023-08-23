699ce535929261b437c8d0e90b87ef7f1652a4cd
Guillaume DALLE <guillaume.dalle74@gmail.com>
2023-08-23 13:49:05 +0200

    bug corection IntallService

--- softwareWeb/InstallService.sh
+++ softwareWeb/InstallService.sh
@@ -1,6 +1,7 @@
 buzzerdir=`pwd`
 echo $buzzerdir
 cat $buzzerdir/buzzer.service | sed "s|BUZZERDIR|${buzzerdir}|g" | sudo tee /etc/systemd/system/buzzer.service
+echo "okkkkkkkkkk"
 sudo chmod 640 /etc/systemd/system/buzzer.service
 sudo systemctl daemon-reload
 sudo systemctl enable buzzer
